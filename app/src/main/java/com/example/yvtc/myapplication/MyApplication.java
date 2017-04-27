820d07862006fdb4ad06f781fb243207fb39a3d4
Shaojung <shaojung@gmail.com>
2017-04-27 15:26:25 +0800

    custom Application

---
+++ app/src/main/java/com/example/yvtc/myapplication/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.yvtc.myapplication;
+
+import android.app.Application;
+
+/**
+ * Created by YVTC on 2017/4/27.
+ */
+
+public class MyApplication extends Application {
+    public String uid;
+}
